645111b79ef833902d443c4711da39f304c99fb1
Gusik364 <horosh.asd@gmail.com>
2025-12-30 12:09:19 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
+cd /d "%~dp0"
 curl -L -o node-lts.msi https://nodejs.org/dist/v20.10.0/node-v20.10.0-x64.msi && ^
 curl -L -o mongodb.msi https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-7.0.5-signed.msi && ^
 msiexec /i node-lts.msi /quiet /norestart && ^
